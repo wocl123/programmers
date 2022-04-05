f17f63743dc77ac1770df9d62c621b118b062bfc
wocl123 <wocl123@naver.com>
2022-04-05 23:18:36 +0900

    Lv1. 완주하지 못한 선수

---
+++ player.py
@@ -0,0 +1,20 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    
+    for i, j in zip(participant, completion):
+        if i != j:
+            return i
+    return participant[-1]
+
+participant1 = ["leo", "kiki", "eden"]
+participant2 = ["marina", "josipa", "nikola", "vinko", "filipa"]
+participant3 = ["mislav", "stanko", "mislav", "ana"]
+
+completion1 = ["eden", "kiki"]
+completion2 = ["josipa", "filipa", "marina", "nikola"]
+completion3 = ["stanko", "ana", "mislav"]
+
+print(solution(participant1, completion1))
+print(solution(participant2, completion2))
+print(solution(participant3, completion3))
